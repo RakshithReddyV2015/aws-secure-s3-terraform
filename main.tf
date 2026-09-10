c384825b4323203ba7b86dabfe293bf7a258b3f5
Dilip Reddy <dilipreddyit@gmail.com>
2026-09-10 14:39:12 -0500

    my intital project

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "data_bucket" {
+  bucket = var.bucket_name
+}
+resource "aws_s3_bucket_versioning" "data_bucket_versioning" {
+  bucket = aws_s3_bucket.data_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
